d68f9fbfa9417c2a6f95fb585bd5f28a64c67a6f
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2023-11-15 13:37:48 -0600

    added js to mark completed or not

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 console.log("hi");
+
+const library = [];
+const addBtn = document.querySelector("#add-btn");
+const completedBtn = Array.from(document.querySelectorAll(".completed"));
+const deleteBtn = Array.from(document.querySelectorAll(".book-delete"));
+
+function Book(title, author, pages, completed) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.completed = completed;
+}
+
+// CLICK THE COMPLETED BTN TO SHOW COMPLETED OR NOT
+completedBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.innerText === "Completed") {
+      e.target.innerText = "Not Completed";
+      e.target.classList.remove("completed");
+      e.target.classList.add("not-completed");
+    } else {
+      e.target.innerText = "Completed";
+      e.target.classList.remove("not-completed");
+      e.target.classList.add("completed");
+    }
+  });
+});
